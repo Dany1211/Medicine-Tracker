138c3be23417c3e4f3bb14c48e721a9d8282bd8a
Dany <dnyanesh2442@gmail.com>
2025-01-08 23:08:53 +0530

    Initial landing page created

--- app/login/index.tsx
+++ app/login/index.tsx
@@ -1,17 +1,33 @@
-import { View, Text, Image } from 'react-native'
-import React from 'react'
-import '@/globals.css'
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import "@/globals.css";
+import { push } from "expo-router/build/global-state/routing";
+import { useRouter } from "expo-router";
 
 const LoginScreen = () => {
+
+const router=useRouter();
+
+
   return (
     <View>
-      <View>
-        <Image source={require('@/assets/images/login.png')}>
-              
-        </Image>
+      <View className="items-center mt-20">
+        <Image
+          source={require("@/assets/images/logoSplash.png")}
+          className="w-[350px] h-[350px] rounded-lg "
+        ></Image>
+      </View>
+      <View className="p-8 bg-blue-500 h-[100%] mt-[80px]">
+        <Text className=" text-white font-bold text-[30px] text-center">Stay on Track,Stay Healthy!</Text>
+        <Text className="text-white text-center text-[18px] mt-[20px]">Track your Meds, take control of your health, stay consistent, stay confident. </Text>
+
+        <TouchableOpacity onPress={()=>router.push('/login/signIn')}>
+            <Text className="p-[15px] bg-white rounded-[50px] text-center mt-10 text-[18px] text-blue-500 font-medium">Continue</Text>
+        </TouchableOpacity>
+        <Text className="text-white mt-2">By clicking continue button, you will agree to our terms and conditions.</Text>
       </View>
     </View>
-  )
-}
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
